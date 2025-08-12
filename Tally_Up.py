e6e7ca1731603119596c68d04ab733daaa3af685
SP6 <subhaanhussain20@gmail.com>
2025-08-12 13:22:05 +0100

    add feature to generate random path which can be used to simulate a random game

--- Tally_Up.py
+++ Tally_Up.py
@@ -2,6 +2,9 @@ import tkinter as tk
 import random
 
 GRID_SIZE = 4
+ITERATIONS_MAX = 64
+ITERATIONS_MIN = 16
+CELL_POSITION = [(0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2),(2,3),(3,0),(3,1),(3,2),(3,3)] 
 
 class GridGame():
     def __init__(self, window):
@@ -23,8 +26,12 @@ class GridGame():
         
         self.message_label = tk.Label(window, text="", font=("Arial", 12))
         self.message_label.grid(row=GRID_SIZE + 1, column=0, columnspan=GRID_SIZE)
-        
+
+        self.generated_selections = []
+        self.generated_neighbours = []
+
         self.build_grid()
+        self.generate_path()
 
     def build_grid(self):
         for i in range(GRID_SIZE):
@@ -33,6 +40,39 @@ class GridGame():
                                 width=4, height=2, command=lambda x=i, y=j: self.cell_clicked(x, y))
                 btn.grid(row=i, column=j, padx=2, pady=2)
                 self.buttons[i][j] = btn
+
+    def generate_path(self): 
+        left = (-1,0)
+        right = (1,0)
+        up = (0,1)
+        down = (0,-1)
+
+        direction = [left,right,up,down]
+        for _ in range(random.randint(ITERATIONS_MIN, ITERATIONS_MAX)):
+            cell = random.choice(CELL_POSITION)
+            x = cell[0]
+            y = cell[1]
+            illegal_neighbour = []
+
+            if (x == 0 or y == 0):
+                if (x == 0 and y == 0):
+                    illegal_neighbour += [left, up]
+                if (x == 0 and y != 0):
+                    illegal_neighbour += [left]
+                if (x != 0 and y == 0):
+                    illegal_neighbour += [up]
+
+            if (0 == 3 or y == 3):
+                if (x == 3 and y == 3):
+                    illegal_neighbour += [right, down]
+                if (x == 3 and y != 3):
+                    illegal_neighbour += [right]
+                if (x != 3 and y == 3):
+                    illegal_neighbour += [down]
+
+            neighbours = [i for i in direction if i not in illegal_neighbour]
+            self.generated_selections.append(cell)
+            self.generated_neighbours.append(random.choice(neighbours))
 
     def cell_clicked(self, x, y):
         self.selection.append((x, y))
@@ -50,7 +90,7 @@ class GridGame():
 
         if abs(x2 - x1) + abs(y2 - y1) != 1:
         #if abs(x2 - x1) | abs(y2 - y1) > 1: # logic to include diagonals 
-            self.message_label.config(text="❌Please select an adjacent cell", fg="red")
+            self.message_label.config(text="x Please select an adjacent cell", fg="red")
             self.clear_selection()
             return
 
